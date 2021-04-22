e6e8ff8a1acdcf2648d312ee1174bc1ad4de3383
tharukshi724 <tharukshiw@gmail.com>
2021-04-22 19:30:44 +0530

    add TharukshiActivity

---
+++ app/src/main/java/com/example/testingmad/TharukshiActivity.java
@@ -0,0 +1,14 @@
+package com.example.testingmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TharukshiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tharukshi);
+    }
+}
