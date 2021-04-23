8cea5ad029066352ee67707021e4b36ee99e3101
iamKAviB <iamkavib@gmail.com>
2021-04-23 10:21:19 +0530

    test

---
+++ app/src/main/java/com/example/testingmad/KaviBhere.java
@@ -0,0 +1,14 @@
+package com.example.testingmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KaviBhere extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kavi_bhere);
+    }
+}
